1e3cb78b52d835202c71badd6efc920d771c9db7
Scott Walker <scott@swlt.ru>
2025-06-17 23:01:01 +0300

    настройка docker

--- .docker/api/entrypoint.sh
+++ .docker/api/entrypoint.sh
@@ -5,4 +5,4 @@ set -e
 chown -R www-data:www-data /var/www/storage /var/www/bootstrap/cache
 chmod -R 775 /var/www/storage /var/www/bootstrap/cache
 
-exec "$@"
+service nginx start
---
+++ .docker/front/entrypoint.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+service nginx start
+
+tail -f /dev/null
