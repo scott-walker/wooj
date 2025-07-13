d9ab3d1815dcb08f9d555478a2f3d4b204a47bad
Scott Walker <scott@swlt.ru>
2025-07-13 18:46:27 +0300

    api. фикс привязки топиков

--- api/app/Http/Resources/WoojResource.php
+++ api/app/Http/Resources/WoojResource.php
@@ -23,6 +23,7 @@ class WoojResource extends JsonResource
             'is_pinned' => $this->isPinned,
             'is_published' => false,
             'is_deleted' => (bool) $this->deleted_at,
+            'topic_ids' => $this->topicIds,
             // 'deleted_at' => $this->deleted_at ? Carbon::parse($this->deleted_at)->format('Y-m-d H:i:s') : null,
             'created_at' => Carbon::parse($this->created_at)->format('Y-m-d H:i:s'),
             'updated_at' => Carbon::parse($this->updated_at)->format('Y-m-d H:i:s'),
