141ea9db9d2c061b6f85851681a4cba88a92b3b3
Scott Walker <scott@swlt.ru>
2025-10-01 17:26:52 +0300

    Обновлен скрипт entrypoint.sh для установки HTTP_PORT по умолчанию на 80. Изменены настройки nginx для использования переменной окружения HTTP_PORT. Закомментирован отладочный код в index.php.

--- api/public/index.php
+++ api/public/index.php
@@ -13,31 +13,31 @@ if (file_exists($maintenance = __DIR__ . '/../storage/framework/maintenance.php'
 // Register the Composer autoloader...
 require __DIR__ . '/../vendor/autoload.php';
 
-if ($_GET['debug'] ?? false) {
-    $vars = getenv();
+// if ($_GET['debug'] ?? false) {
+//     $vars = getenv();
 
-    echo '<pre>';
-    print_r([
-        'APP_ENV' => $vars['APP_ENV'] ?? "",
-        'APP_DEBUG' => $vars['APP_DEBUG'] ?? "",
-        'APP_URL' => $vars['APP_URL'] ?? "",
-        'APP_FRONT_URL' => $vars['APP_FRONT_URL'] ?? "",
-        'DB_HOST' => $vars['DB_HOST'] ?? "",
-        'DB_PORT' => $vars['DB_PORT'] ?? "",
-        'DB_NAME' => $vars['DB_NAME'] ?? "",
-        'DB_USER' => $vars['DB_USER'] ?? "",
-        'DB_PASS' => $vars['DB_PASS'] ?? "",
-        'MAIL_MAILER' => $vars['MAIL_MAILER'] ?? "",
-        'MAIL_SCHEME' => $vars['MAIL_SCHEME'] ?? "",
-        'MAIL_HOST' => $vars['MAIL_HOST'] ?? "",
-        'MAIL_PORT' => $vars['MAIL_PORT'] ?? "",
-        'MAIL_USERNAME' => $vars['MAIL_USERNAME'] ?? "",
-        'MAIL_PASSWORD' => $vars['MAIL_PASSWORD'] ?? "",
-        'MAIL_FROM_ADDRESS' => $vars['MAIL_FROM_ADDRESS'] ?? "",
-    ]);
-    echo '</pre>';
-    exit;
-}
+//     echo '<pre>';
+//     print_r([
+//         'APP_ENV' => $vars['APP_ENV'] ?? "",
+//         'APP_DEBUG' => $vars['APP_DEBUG'] ?? "",
+//         'APP_URL' => $vars['APP_URL'] ?? "",
+//         'APP_FRONT_URL' => $vars['APP_FRONT_URL'] ?? "",
+//         'DB_HOST' => $vars['DB_HOST'] ?? "",
+//         'DB_PORT' => $vars['DB_PORT'] ?? "",
+//         'DB_NAME' => $vars['DB_NAME'] ?? "",
+//         'DB_USER' => $vars['DB_USER'] ?? "",
+//         'DB_PASS' => $vars['DB_PASS'] ?? "",
+//         'MAIL_MAILER' => $vars['MAIL_MAILER'] ?? "",
+//         'MAIL_SCHEME' => $vars['MAIL_SCHEME'] ?? "",
+//         'MAIL_HOST' => $vars['MAIL_HOST'] ?? "",
+//         'MAIL_PORT' => $vars['MAIL_PORT'] ?? "",
+//         'MAIL_USERNAME' => $vars['MAIL_USERNAME'] ?? "",
+//         'MAIL_PASSWORD' => $vars['MAIL_PASSWORD'] ?? "",
+//         'MAIL_FROM_ADDRESS' => $vars['MAIL_FROM_ADDRESS'] ?? "",
+//     ]);
+//     echo '</pre>';
+//     exit;
+// }
 
 // Bootstrap Laravel and handle the request...
 /** @var Application $app */
